2ae7faeef882b90c59a6f05d5de29c6a1adca9ed
Aundrei Bryant <drei97@gmail.com>
2023-01-18 11:40:01 -0600

    WIP

--- app/index.js
+++ app/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 
 import studentRoutes from "./student/routes.js";
+const app = express();
 
 // * This is a middleware
 // Tell express to parse the request body as JSON
 // Without this, req.body will be undefined
 app.use(express.json());
 
-const app = express();
 // this is a middleware
 // any request that starts with /api/students will be handled by studentRoutes
-app.use("/api/students", studentRoutes);
+app.use("/api/students/", studentRoutes);
 
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
